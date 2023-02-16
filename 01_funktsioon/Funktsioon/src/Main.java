ff190a0139b54521a6c858a97fde20cf8493108e
Adrian Käsper <adriank@tlu.ee>
2023-02-16 17:16:14 +0200

    1. kodutöö tehtud

--- 01_funktsioon/Funktsioon/src/Main.java
+++ 01_funktsioon/Funktsioon/src/Main.java
@@ -1,5 +1,36 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        int EAP = 180;
+        int hours = 79;
+
+        System.out.println(EAP + " EAP = " + GetEAPWorkTime(EAP) + " tundi tööd.");
+        System.out.println(hours + " tundi = " + GetHoursFromDays(hours) + " päeva.");
+        System.out.println();
+        MultiplicationTable();
+
+
     }
+
+    private static void MultiplicationTable() {
+        System.out.println("Korrutustabel:");
+        for (int x = 1; x < 11; x++) {
+            if (x != 1) {
+                System.out.println();
+            }
+            for (int y = 1; y < 11; y++) {
+                int number = x*y;
+                System.out.format("%-4d", number);
+            }
+        }
+    }
+
+    private static int GetEAPWorkTime(int EAP) {
+        return EAP*26;
+    }
+    private static double GetHoursFromDays(int hours) {
+        return (double) hours /24;
+    }
+
+
 }
