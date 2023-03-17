8b70d76dc6eca6f07842340d1d9cff9acf84e89c
Adrian Käsper <adriank@tlu.ee>
2023-03-17 13:02:16 +0200

    13.00 upload

--- 05_kontrolltoo1/kontrolltoo/src/Main.java
+++ 05_kontrolltoo1/kontrolltoo/src/Main.java
@@ -1,5 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int kiirus1 = 60;
+        int kiirus2 = 50;
+        System.out.println(kiirus1 + " km/h ja " + kiirus2 + " km/h keskmine on " + kaheKeskmine(kiirus1, kiirus2));
+
+
     }
+    public static double kaheKeskmine(double kiirus1, double kiirus2) {
+        double keskmineKiirus = (kiirus1 + kiirus2) / 2;
+        return keskmineKiirus;
+    }
+    public static double listiKeskmine(double kiirus1, double kiirus2) {
+
+        return 1;
+    }
+
 }
