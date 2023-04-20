f7d52ecd77013c04d85805c1ee0b83df89401f0a
Adrian Käsper <adriank@tlu.ee>
2023-04-20 16:46:47 +0300

    Kodutöö tehtud

---
+++ 09_tunnirakenduse_lahendus/Primitiivid/src/main/java/ee/adrian/primitiivid/Toode.java
@@ -0,0 +1,16 @@
+package ee.adrian.primitiivid;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Toode {
+    private int id;
+    private String nimi;
+    private double hind;
+    private boolean aktiivne;
+}
